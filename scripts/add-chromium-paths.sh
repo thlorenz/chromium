eb035e087650034abcf0a282695cf246b809cf66
Thorsten Lorenz <thlorenz@gmx.de>
2014-09-25 21:51:24 -0400

    adding path exports

---
+++ scripts/add-chromium-paths.sh
@@ -0,0 +1,5 @@
+# source this file (don't run it)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PATH=$DIR/../../src/third_party/llvm-build/Release+Asserts/bin:$DIR/../../depot_tools:$PATH
